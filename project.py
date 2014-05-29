838be06ba4e70569e2d50b463de273d7f18fb526
Rodny Perez <rodnyeperez@gmail.com>
2014-05-29 15:48:04 -0400

    Began writting the simulator project.

--- project.py
+++ project.py
@@ -1,5 +1,34 @@
-#Comment
-print "Hello world"
+"""
+This is the project
+"""
+import random
+#random generator, uses seeds for testing purposes if you want repeatability
+random_generator = random.Random(1) 
+number_of_sensors = 5
+number_of_readings = 10
+actual_temperature = 20
+sensor_standard_deviation = 5
 
-for x in xrange(1,11):
-	print x
+class Sensor(object):
+	"""
+	Represents a sensor and can be polled for data
+	"""
+	def __init__(self, mean, sd, ran_gen):
+		self.mean = mean
+		self.sd = sd
+		self.ran_gen = ran_gen
+
+	def get_reading(self):
+		return self.ran_gen.normalvariate(self.mean, self.sd)
+
+def get_time_series(sensor, number_of_readings):
+	" pull sensor for how many readings is going to do "
+	data = []
+	for x in range(number_of_readings):
+		data.append(sensor.get_reading())
+	return data
+
+
+s = Sensor(actual_temperature, sensor_standard_deviation, random_generator)
+data = get_time_series(s, number_of_readings)
+print data
