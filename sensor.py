d2b2e52ce3241bfc88dd13f30e00f099225ee23f
Rodny Perez <rodnyeperez@gmail.com>
2014-06-05 14:26:39 -0400

    Rewrote class so that time series can be added from the constructor.

--- sensor.py
+++ sensor.py
@@ -2,28 +2,8 @@ class Sensor(object):
 	"""
 	Represents a sensor and can be polled for data
 	"""
-	def __init__(self, mean, sd, sensor_id, ran_gen, type_of_sensor="NA"):
-		self.mean = mean
-		self.sd = sd
-		self.ran_gen = ran_gen
-		self.sensor_id = sensor_id
-		self.online_status = 1	#to check the status of the sensor. 0 offline 1 online
-		self.type_of_sensor = type_of_sensor
+	def __init__(self, time_series):
+		self.time_series = time_series
 
-	def get_reading(self):
-		return self.ran_gen.normalvariate(self.mean, self.sd)
-
-	def get_online_status(self):
-		return self.online_status
-
-	def set_online_status(self, status):
-		"""Use it to change the status of the sensor"""
-		self.online_status = status
-
-	def get_sensor_id(self):
-		return self.sensor_id
-
-	def __str__(self):
-		return "Sensor " + self.sensor_id + ":\nType of sensor: " + \
-		str(self.type_of_sensor) + "\nMean: " + str(self.mean) + \
-		"\nSd: " + str(self.sd) + "\nStatus: " + str(self.online_status)
+	def get_time_series(self):
+		return self.time_series
