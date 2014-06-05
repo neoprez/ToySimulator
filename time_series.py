b09d0dd9ba4cbaf92bd98f34fe757bf5685f9667
Rodny Perez <rodnyeperez@gmail.com>
2014-06-05 12:16:15 -0400

    added merge time series

--- time_series.py
+++ time_series.py
@@ -33,11 +33,48 @@ def normalize_to_range(time_series, desired_max=100):
 
 	return [(curr_number + min_coef) * max_coef for curr_number in time_series]
 
+def merge_series(list_of_time_series, list_of_weights):
+	"""
+	This function merges a list of series into one according to their weight.
+	"""
+	merged_series = [0] * len(list_of_time_series[0])
 
+	for idx in range(len(list_of_time_series)):
+		coefficient_of_multiplication = list_of_weights[idx]
+		series = list_of_time_series[idx]
+		for index in range(len(series)):
+			merged_series[index] = merged_series[index] + \
+			(series[index] * coefficient_of_multiplication)
+	return merged_series
 
 
 time_series = generate_time_series(1000)
+time_series_b = generate_time_series(1000)
+time_series_c = generate_time_series(1000)
 normalized_time_series = normalize_to_range(time_series)
+normalized_time_series_b = normalize_to_range(time_series_b)
+normalized_time_series_c = normalize_to_range(time_series_c)
 
-plt.plot(normalized_time_series)
+merged_series = merge_series([normalized_time_series, normalized_time_series_b, 
+	normalized_time_series_c], [0.25, 0.25, 0.5])
+
+normalized_merged_series = normalize_to_range(merged_series)
+
+figure = plt.figure()
+
+width = 1
+height = 4
+
+series_a_axes = figure.add_subplot(height, width, 1)
+series_a_axes.plot(normalized_time_series)
+
+series_b_axes = figure.add_subplot(height, width, 2)
+series_b_axes.plot(normalized_time_series_b)
+
+series_b_axes = figure.add_subplot(height, width, 3)
+series_b_axes.plot(normalized_time_series_c)
+
+merged_series_axes = figure.add_subplot(height, width, 4)
+merged_series_axes.plot(normalized_merged_series)
+
 plt.show()
