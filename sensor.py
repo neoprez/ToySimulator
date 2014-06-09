c4f49a8bb716ccb0d0c2d34e9fd8bb6900ddb497
Rodny Perez <rodnyeperez@gmail.com>
2014-06-09 14:19:34 -0400

    Added a method that returns the reading at a particular time

--- sensor.py
+++ sensor.py
@@ -10,3 +10,7 @@ class Sensor(object):
 
 	def set_time_series(self, time_series):
 		self.time_series = time_series
+
+	def get_reading_at_time(self, time):
+		"Returns the value in the time series at a particular time"
+		return self.time_series[time]
